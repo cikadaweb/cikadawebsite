c7ed33ddc1b5aa66514667a15c3a7744e4fdfe13
CIKADA <adel.garipovazn@mail.ru>
2023-01-14 11:08:52 +0300

    Add RecentPosts section

---
+++ components/AppRecentPost.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="recent-post">
+    <div class="recent-post__header">
+      <h2 class="recent-post__title">
+        {{ item.title }}
+      </h2>
+      <div class="recent-post__info">
+        <div class="recent-post__info-date">
+          {{ item.publish_date }}
+        </div>
+        <div class="recent-post__info-tag">
+          {{ item.tag }}
+        </div>
+      </div>
+    </div>
+    <div class="recent-post__body">
+      <div class="recent-post__text">
+        {{ item.text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  defineProps, PropType,
+} from 'vue';
+
+interface IPropsPost {
+  id: number,
+  title: string,
+  publish_date: string,
+  tag: string,
+  text: string,
+}
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<IPropsPost>,
+    required: true,
+  },
+});
+
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+.recent-post {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background: $white-color;
+  padding: 25px;
+  box-shadow: 0px 4px 10px rgba(187, 225, 250, 0.25);
+  border-radius: 4px;
+}
+.recent-post__header {
+}
+.recent-post__body {
+
+}
+.recent-post__title {
+  @include font(26px, 700, 38px);
+  color: $blue-dark-color;
+}
+.recent-post__info {
+  display: flex;
+  margin-bottom: 10px;
+
+}
+.recent-post__info-date {
+  padding-right: 20px;
+  border-right: 1px solid $black-color;
+}
+.recent-post__info-tag {
+  padding-left: 20px;
+  border-right: 1px solid transparent;
+}
+.recent-post__text {
+
+}
+</style>
---
+++ components/RecentPosts.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="recent-posts">
+    <div class="recent-posts__nav">
+      <div class="recent-posts__title">
+        Recent posts
+      </div>
+      <nuxt-link class="recent-posts__link" to="/blog">View all</nuxt-link>
+    </div>
+    <div class="recent-posts__row">
+      <div class="recent-posts__column" v-for="post in posts" :key="post.id">
+        <AppRecentPost :item="post" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const posts = ref([
+  {
+    id: 1,
+    title: 'Entity Framework в WinForms',
+    publish_date: '6 Января 2023',
+    tag: 'Windows Forms',
+    text: 'На сегодняшний день Winforms все еще остается платформой для многих бизнес-приложений, ориентированных на работу с данными, и привязка данных является неотъемлемой частью разработки для Winforms. ',
+  },
+  {
+    id: 2,
+    title: 'Vuetify как начать работать',
+    publish_date: '30 Декабря 2022',
+    tag: 'Vue, Vuetify',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+  }
+]);
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+.recent-posts {
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+.recent-posts__nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.recent-posts__title {
+  @include font(22px, 400, 60px);
+  color: $blue-dark-color;
+}
+.recent-posts__link {
+  display: inline-block;
+  @include font(16px, 400, 24px);
+  color: $blue-light-color2;
+  text-decoration: none;
+}
+.recent-posts__row {
+  display: flex;
+  margin: 0 -10px;
+}
+.recent-posts__column {
+  width: calc(100% / 10) * 5;
+  padding: 0 10px;
+}
+</style>
