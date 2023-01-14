3349288747219c6f09fe11986df0217c95396052
CIKADA <adel.garipovazn@mail.ru>
2023-01-14 11:09:26 +0300

    Add AppContainer

---
+++ components/AppContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1024px;
+  padding: 0 148px;
+  margin: 0 auto;
+}
+</style>
