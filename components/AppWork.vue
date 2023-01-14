4a285058bd5a3accdc20c243743663e905e3bae8
CIKADA <adel.garipovazn@mail.ru>
2023-01-14 11:09:15 +0300

    Add FeaturedWorks section

---
+++ components/AppWork.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="work-card">
+
+    <div class="work-card__image">
+      <v-picture :classNameContainer="'work-card__picture'" :classNameImage="'work-card__img'"
+        :imageUrl="item.image_url" />
+    </div>
+
+    <div class="work-card__details">
+      <h2 class="work-card__title">{{ item.title }}</h2>
+      <span class="work-card__info__badge">{{ item.publish_date }}</span>
+      <span class="work-card__info__tag">{{ item.tag }}</span>
+      <div class="work-card__info__text">
+        {{ item.text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  defineProps, PropType,
+} from 'vue';
+
+interface IPropsWork {
+  id: number,
+  title: string,
+  publish_date: string,
+  tag: string,
+  text: string,
+  image_url: string,
+}
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<IPropsWork>,
+    required: true,
+  },
+});
+
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+.work-card {
+  display: flex;
+  padding-bottom: 15px;
+}
+
+.work-card__image {
+  width: calc(100% / 10) * 3;
+  padding-right: 9px; // ToDo
+}
+
+.work-card__details {
+  width: calc(100% / 10) * 7;
+  padding-left: 9px; // ToDo
+}
+
+.work-card__title {
+  @include font(30px, 700, 44px);
+  color: $blue-dark-color;
+  margin: 0;
+  margin-bottom: 16px;
+}
+
+.work-card__info__badge {
+  @include font(18px, 900, 60px);
+  color: $white-color;
+  background: $blue-dark-color;
+  padding: 2px 10px;
+  border-radius: 16px;
+}
+
+.work-card__info__tag {
+  @include font(20px, 400, 29px);
+  color: $gray-light-color1;
+  margin-left: 20px;
+}
+
+.work-card__info__text {
+  @include font(16px, 400, 24px);
+  color: $blue-dark-color;
+}
+</style>
---
+++ components/FeaturedWorks.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="featured-works">
+
+    <div class="featured-flag">
+
+    </div>
+
+    <div class="featured-works__title">
+      Featured works
+    </div>
+
+    <div class="featured-works__row">
+      <div class="featured-works__column" v-for="work in works" :key="work.id">
+        <AppWork :item="work"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const works = ref([
+  {
+    id: 1,
+    title: 'Promo страница Mercedes Benz',
+    publish_date: '2020',
+    tag: 'Promo',
+    text: 'Промо страница продажи новой модели Mercedez бизнес класса!',
+    image_url: 'promo-mercedes.png',
+  },
+  {
+    id: 2,
+    title: 'Сервис доставки еды на дом',
+    publish_date: '2021',
+    tag: 'Интернет-магазин',
+    text: 'Рестораны различных кухонь всего за 30 минут.',
+    image_url: 'delivery-food.png',
+  }
+]);
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+.featured-works {
+  padding-bottom: 18px;
+  margin-bottom: 83px; // ToDo
+}
+.featured-flag { //ToDo
+  width: 11px;
+  height: 36px;
+  left: 149px;
+  top: 991px;
+
+  background: #C4C4C4;
+}
+.featured-works__title {
+  @include font(22px, 400, 60px);
+  color: $blue-dark-color;
+  margin-bottom: 20px;
+}
+.featured-works__row {
+  display: flex;
+  flex-direction: column;
+}
+.featured-works__column {
+  border-bottom: 1px solid $gray-light-color2;
+  margin-bottom: 30px;
+}
+</style>
