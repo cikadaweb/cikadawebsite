0cd82a465d7b554b6e79b72b67e0a6443d94475a
CIKADA <adel.garipovazn@mail.ru>
2023-01-15 16:07:12 +0300

    Add tablet styles

--- components/AppContainer.vue
+++ components/AppContainer.vue
@@ -5,9 +5,14 @@
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
 .container {
   max-width: 1024px;
   padding: 0 148px;
   margin: 0 auto;
+  @include for-size(tablet) {
+    padding: 0 30px;
+  }
 }
 </style>
