bc104ecf3030832dd65891b984c6e841a4fe672c
CIKADA <adel.garipovazn@mail.ru>
2023-01-29 18:17:14 +0300

    Add Blog page

---
+++ components/Blog.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="blog">
+    <h1 class="blog__title">Блог</h1>
+    <div class="blog__row">
+      <div class="blog__column" v-for="post in posts" :key="post.id">
+        <AppRecentPost :item="post" isBlogPage/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    try {
+      await this.$store.dispatch('posts/fetchPosts');
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  computed: {
+    posts() {
+      return this.$store.getters['posts/getPosts'];
+    }
+  }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+.blog__title {
+  @include font(44px, 700, 60px);
+    margin: 0 0 40px;
+  @include for-size(tablet) {
+    @include font(30px, 700, 60px);
+    margin-top: 35px;
+  }
+}
+
+.blog__row {
+  display: flex;
+  flex-direction: column;
+}
+
+.blog__column {
+
+}
+
+.blog__column+.blog__column {
+  @include for-size(tablet) {
+    margin-top: 30px;
+  }
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <AppContainer>
+      <Blog />
+    </AppContainer>
+  </div>
+</template>
