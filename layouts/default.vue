57c7950c1e961072fe0569254d977a91c7a161f9
CIKADA <adel.garipovazn@mail.ru>
2023-01-29 18:13:30 +0300

    Create custom layouts

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppContainer>
+      <TheHeader />
+    </AppContainer>
+    <Nuxt />
+    <AppContainer>
+      <TheFooter />
+    </AppContainer>
+  </div>
+</template>
+
+<script lang="ts">
+import AppContainer from '@/components/AppContainer.vue';
+import TheHeader from '@/components/header/TheHeader.vue';
+import TheFooter from '@/components/TheFooter.vue';
+
+import Vue from 'vue'
+export default Vue.extend({
+  components: {
+    AppContainer, TheHeader, TheFooter,
+  }
+})
+</script>
