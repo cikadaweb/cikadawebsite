5cdc52225e7e0eb86671c396f351a9436a786646
CIKADA <adel.garipovazn@mail.ru>
2023-02-13 22:16:14 +0300

    [WIP] Change AppBreadcrumb

---
+++ components/breadcrumb/AppBreadcrumb.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="breadcrumb">
+    <ul class="breadcrumb__list">
+      <li class="breadcrumb__item">
+        <nuxt-link class="breadcrumb__link" to="/" >Главная</nuxt-link>
+      </li>
+      <li class="breadcrumb__item">
+        <nuxt-link class="breadcrumb__link" to="/">Каталог</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+
+import { defineProps, toRefs } from 'vue';
+
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+
+.breadcrumb {
+  margin-bottom: 20px;
+}
+
+.breadcrumb__list {
+  display: flex;
+  padding: 0 0;
+  margin: 0 0;
+  list-style: none;
+}
+
+.breadcrumb__item {
+  @include font(14px, 400, 140%);
+}
+
+.breadcrumb__item::after {
+  content: '/';
+  margin: 0 2px;
+}
+
+.breadcrumb__item:last-child::after {
+  content: '';
+}
+
+.breadcrumb__link {
+  color: inherit;
+  text-decoration: none;
+}
+
+.breadcrumb__link:hover {
+  text-decoration: underline;
+}
+</style>
