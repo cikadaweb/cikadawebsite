6f56efd1c8d6e1a1c4f1a27bfe5b00325db60cb7
CIKADA <adel.garipovazn@mail.ru>
2023-02-13 22:12:52 +0300

    Change Post page

---
+++ components/pages/Post.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="post">
+    <AppBreadcrumb />
+    <AppPost :item="post" />
+  </div>
+</template>
+
+<script>
+import AppBreadcrumb from '@/components/breadcrumb/AppBreadcrumb.vue';
+import AppPost from '@/components/posts/AppPost.vue';
+
+export default {
+  async fetch() {
+    try {
+      await this.$store.dispatch('posts/fetchCurrentPost', 1);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  computed: {
+    post() {
+      return this.$store.getters['posts/getCurrentPost'];
+    }
+  },
+  components: {
+    AppBreadcrumb, AppPost,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+</style>
