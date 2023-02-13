7728a8c3a590f18d4a449ea036e0941d57b8417f
CIKADA <adel.garipovazn@mail.ru>
2023-02-13 22:17:30 +0300

    [WIP] Create Comment components

---
+++ components/posts/AppCommentForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="comment-form">
+    <div>Добавить комментарий</div>
+    <form action="#" method="POST" @submit.prevent="postComment">
+      
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {
+  defineProps, PropType,
+} from 'vue';
+
+const postComment = () => {
+  console.log('postComment: ');
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+</style>
