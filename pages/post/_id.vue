f89f7d060fb2a432c13ecee9aeb7ed2f5e937654
CIKADA <adel.garipovazn@mail.ru>
2023-02-24 23:52:55 +0300

    Add Post page validate

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -6,13 +6,18 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import AppContainer from '@/components/AppContainer.vue';
 import Post from '@/components/pages/Post.vue';
 
-import { useRoute } from '@nuxtjs/composition-api'
-
-const route = useRoute();
-
+import Vue from 'vue'
+export default Vue.extend({
+  validate({ params }) {
+    return Boolean(params.id)
+  },
+  components: {
+    AppContainer, Post
+  }
+})
 </script>
 
