6cc69d806df5f5b289603853a35f2d0e13b77f57
CIKADA <adel.garipovazn@mail.ru>
2023-09-07 23:14:10 +0300

    Refactor color variables

--- components/posts/AppRecentPost.vue
+++ components/posts/AppRecentPost.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="recent-post" :class="isBlogPage ? 'recent-post_page' : ''">
     <div class="recent-post__header">
-      <nuxt-link class="recent-post__title" :to="`/post/${item.id}`">
+      <Nuxt-link class="recent-post__title" :to="`/post/${item.id}`">
         {{ item.title }}
-      </nuxt-link>
+      </Nuxt-link>
       <div class="recent-post__info">
         <div class="recent-post__info-date">
           {{ item.publish_date }}
@@ -26,7 +26,9 @@ import {
   defineProps, PropType,
 } from 'vue';
 
-import { useRouter } from '@nuxtjs/composition-api'
+import { useRouter } from '@nuxtjs/composition-api';
+
+const router = useRouter();
 
 interface IPropsPost {
   id: number,
@@ -47,8 +49,6 @@ const props = defineProps({
   }
 });
 
-const router = useRouter();
-
 const openPost = () => {
   router.push(`/post/1`)
 }
@@ -56,12 +56,13 @@ const openPost = () => {
 </script>
 
 <style lang="scss" scoped>
+
 .recent-post {
   position: relative;
   display: flex;
   flex-direction: column;
   height: 100%;
-  background: $white-color;
+  background-color: $bg-primary;
   padding: 25px;
   box-shadow: 0px 4px 10px rgba(187, 225, 250, 0.25);
   border-radius: 4px;
@@ -69,15 +70,17 @@ const openPost = () => {
 
 .recent-post_page {
   padding: 0 0 30px;
-  border-bottom: 1px solid $gray-light-color2;
+  border-bottom: 1px solid $border-gray-light;
   margin-bottom: 30px;
   box-shadow: none;
 }
+
 .recent-post__header {
 }
-.recent-post__body {
 
+.recent-post__body {
 }
+
 .recent-post__title {
   display: inline-block;
   max-width: 100%;
@@ -89,6 +92,7 @@ const openPost = () => {
     @include font(22px, 700, 30px);
   }
 }
+
 .recent-post__title::after {
   content: '';
   position: absolute;
@@ -97,6 +101,7 @@ const openPost = () => {
   top: 0;
   left: 0;
 }
+
 .recent-post__info {
   @include font(18px, 400, 26px);
   display: flex;
@@ -105,14 +110,17 @@ const openPost = () => {
     @include font(16px, 400, 24px);
   }
 }
+
 .recent-post__info-date {
   padding-right: 20px;
-  border-right: 1px solid $black-color;
+  border-right: 1px solid $border-black;
 }
+
 .recent-post__info-tag {
   padding-left: 20px;
   border-right: 1px solid transparent;
 }
+
 .recent-post__text {
   @include font(16px, 400, 24px);
 }
